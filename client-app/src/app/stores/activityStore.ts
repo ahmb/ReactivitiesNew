ed7402045f2c23e2b6e812f2501a5935317f4c02
arb <raufguy@Gmail.com>
2020-08-10 17:48:47 -0400

    Refactoring into async await

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,34 +1,37 @@
-import {observable, action, isAction} from 'mobx';
-import { createContext } from 'react';
-import { IActivity } from '../models/activity';
-import agent from '../api/agent';
-
+import { observable, action, isAction } from "mobx";
+import { createContext } from "react";
+import { IActivity } from "../models/activity";
+import agent from "../api/agent";
 
 class ActivityStore {
-    @observable activities: IActivity[] = [];
-    @observable loadingInitial = false;
-    @observable selectedActivity: IActivity | undefined = undefined;
-    @observable editMode = false;
+  @observable activities: IActivity[] = [];
+  @observable loadingInitial = false;
+  @observable selectedActivity: IActivity | undefined = undefined;
+  @observable editMode = false;
 
-    //need an action that modifies the state of an observable
-    @action loadActivities = () => {
-        this.loadingInitial = true;
-        agent.Activities.list()
-        .then((activities) => {
-          activities.forEach((activity) => {
+  //need an action that modifies the state of an observable
+  //implemented using async await
+  @action loadActivities = async () => {
+    this.loadingInitial = true;
+    try{
+        const activities = await agent.Activities.list();
+        activities.forEach((activity) => {
             //this code removes the 6 levels of accuracy in the datetime value
             activity.date = activity.date.split(".")[0];
             //use this below to reference clas sproperties
-            this.activities.push(activity);
-          });
-        })
-        .finally(() => this.loadingInitial = false);
+            this.activities.push(activity)
+        });
+        this.loadingInitial = false;
+    }catch(error ){
+        console.log(error);
+        this.loadingInitial = false;
     }
+  };
 
-    @action selectActivity = (id: string) => {
-        this.selectedActivity = this.activities.find(a=>a.id === id);
-        this.editMode = false;
-    }
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find((a) => a.id === id);
+    this.editMode = false;
+  };
 }
 
 export default createContext(new ActivityStore());
