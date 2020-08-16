0f48e61c5af6b0034e2659c0c4753be4d6f8e6d1
arb <raufguy@Gmail.com>
2020-08-15 21:14:58 -0400

    grouping activities by date via reduce function

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { Item, Button, Label } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import ActivityStore from "../../../app/stores/activityStore";
+import { IActivity } from "../../../app/models/activity";
+
+const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
+  const activityStore = useContext(ActivityStore);
+  const { deleteActivity, submitting, target } = activityStore;
+
+  return (
+    <Item key={activity.id}>
+      <Item.Content>
+        <Item.Header as="a">{activity.title}</Item.Header>
+        <Item.Meta>{activity.date}</Item.Meta>
+        <Item.Description>
+          <div>{activity.description}</div>
+          <div>
+            {activity.city}, {activity.venue}
+          </div>
+        </Item.Description>
+        <Item.Extra>
+          <Button
+            as={Link}
+            to={`/activities/${activity.id}`}
+            // onClick={() => selectActivity(activity.id)}
+            floated="right"
+            content="View"
+            color="blue"
+          />
+          <Button
+            name={activity.id}
+            loading={target === activity.id && submitting}
+            onClick={(e) => deleteActivity(e, activity.id)}
+            floated="right"
+            content="Delete"
+            color="red"
+          />
+          <Label basic content={activity.category} />
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  );
+};
+
+export default observer(ActivityListItem);
