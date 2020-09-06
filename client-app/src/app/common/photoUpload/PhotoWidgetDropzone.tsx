99ee37e796ee1c9cabb2b83ca1c8ded96053874f
arb <raufguy@Gmail.com>
2020-09-05 22:52:39 -0400

    styling dropzone

--- client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -1,27 +1,45 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { Icon, Header } from "semantic-ui-react";
 
 interface IProps {
   setFiles: (files: object[]) => void;
 }
 
+const dropzoneStyles = {
+  border: "dashed 3px",
+  borderColor: "#eee",
+  borderRadius: "5px",
+  paddingTop: "30px",
+  textAlign: "center" as "center",
+  height: "200px",
+};
+
+const dropzoneActive = {
+  borderColor: "green",
+};
+
 const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
   const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
-    setFiles(acceptedFiles.map((file: object) =>
-      Object.assign(file, { preview: URL.createObjectURL(file) })
-    ));
+    setFiles(
+      acceptedFiles.map((file: object) =>
+        Object.assign(file, { preview: URL.createObjectURL(file) })
+      )
+    );
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div
+      {...getRootProps()}
+      style={
+        isDragActive ? { ...dropzoneStyles, ...dropzoneActive } : dropzoneStyles
+      }
+    >
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+      <Icon name="upload" size="huge" />
+      <Header content="Drop image here" />
     </div>
   );
 };
