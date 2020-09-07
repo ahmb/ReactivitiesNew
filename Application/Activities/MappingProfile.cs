593b216037239db3c7ed5b3b7e242ecb1bf8ce7e
arb <raufguy@Gmail.com>
2020-09-07 19:37:43 -0400

    adding a custom resolver for Automapper

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -16,7 +16,9 @@ namespace Application.Activities
                 destinationMember => destinationMember.DisplayName, options => options.MapFrom(source => source.AppUser.DisplayName))
             .ForMember(
                 d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url)
-            );
+            )
+            //custom value resolver 
+            .ForMember(d => d.Following, o => o.MapFrom<FollowingResolver>());
 
         }
     }
