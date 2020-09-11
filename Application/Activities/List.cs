57000f8c19235c3dc7347722ef12a81eb613dff5
arb <raufguy@Gmail.com>
2020-09-11 19:25:09 -0400

    Filtering the activity list in the API

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -20,23 +22,31 @@ namespace Application.Activities
 
         public class Query : IRequest<ActivitiesEnvelope>
         {
-            public Query(int? limit, int? offset)
+            public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
             {
+
                 Limit = limit;
                 Offset = offset;
-
+                IsGoing = isGoing;
+                IsHost = isHost;
+                StartDate = startDate ?? DateTime.Now;
             }
             public int? Limit { get; set; }
             public int? Offset { get; set; }
+            public bool IsGoing { get; }
+            public bool IsHost { get; }
+            public DateTime? StartDate { get; }
         };
 
         public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
@@ -44,7 +54,23 @@ namespace Application.Activities
             //handler that returns a list all the activities in the database context
             public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var queryable = _context.Activities.AsQueryable();
+                var queryable = _context.Activities
+                .Where(x => x.Date >= request.StartDate)
+                .OrderBy(x => x.Date)
+                .AsQueryable();
+
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable
+                        .Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+                }
+
+                if (request.IsHost && !request.IsGoing)
+                {
+                    queryable = queryable
+                        .Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
+                }
+
 
                 var activities = await queryable
                     .Skip(request.Offset ?? 0)
