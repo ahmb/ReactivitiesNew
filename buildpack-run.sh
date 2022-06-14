a0904d08e241b28277e70af93e062df2907925d1
arb <raufguy@Gmail.com>
2022-06-13 21:46:49 -0400

    updated  buildpack file again

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,1 +1,3 @@
-sudo cp -R /app/API/wwwroot /app/heroku_output >> buildPackError.log
+#!/bin/bash
+#https://github.com/weibeld/heroku-buildpack-run/issues/9
+cp -R /app/API/wwwroot /app/heroku_output >> buildPackError.log
