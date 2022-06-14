e27fb0320c5d50ef232d525a8eb715970f728993
arb <raufguy@Gmail.com>
2022-06-13 22:12:08 -0400

    troubleshooting

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #https://github.com/weibeld/heroku-buildpack-run/issues/9
-cp -R /app/API/wwwroot /app/heroku_output >> buildPackError.log
+echo "$(pwd)"
+echo "$(ls)"
+cp -R API/wwwroot heroku_output >> buildPackError.log
